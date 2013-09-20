0e95ef36ddb4ff8c79ee965cfdfa51327a0d7252
Thomas CARO <mcarothomas@gmail.com>
2013-09-20 15:29:59 +0200

    initial commit

---
+++ src/main/java/taatp1/App.java
@@ -0,0 +1,12 @@
+package taatp1;
+
+/**
+ * THOMAS CAROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World!");
+    }
+}
